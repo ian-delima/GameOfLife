639ea738ee10691a7a21a8a2341a328141f6180c
unknown <CONRADO.CANZANI@correo.ucu.edu.uy>
2025-09-08 10:21:37 -0300

    version con console clear

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Ucu.Poo.GameOfLife;
 
 namespace Program
@@ -11,6 +12,7 @@ namespace Program
             GameBoard game = new GameBoard(20, 20, reader.FileReader()); // Generamos nueva GameBoard y le pasamos la matriz leída desde el FileReader
             for (int i = 0; i < 50; i++) // 50 veces
             {
+                Console.Clear();
                 GamePrinter printer = new GamePrinter(game.NewGen()); // Hacemos un nuevo GamePrinter con la matriz de la nueva generación
                 printer.PrintMatrix(); // Lo mandamos a imprimir
                 Task.Delay(500).Wait(); // Esperamos medio segundo para ver la diferencia
